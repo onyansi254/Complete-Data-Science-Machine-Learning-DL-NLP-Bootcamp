534b362de8470deef3f34e91666cd8dc30733dfb
Rihard Otwoma <ronyansi11@gmail.com>
2025-04-15 23:19:01 +0300

    importing modules and packages

---
+++ modules/package/__init__.py
@@ -0,0 +1,1 @@
+# this is a special file used in oython to define packages and initialize thier namespaces
---
+++ modules/package/test.py
@@ -0,0 +1,7 @@
+from maths import *
+from subpackages.multi import multiply
+
+print(addition(2, 3))
+
+
+print(multiply(2, 3))
