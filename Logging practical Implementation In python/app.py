d217360b2f819f4155ef39416d257d34d6b59b88
Rihard Otwoma <ronyansi11@gmail.com>
2025-05-07 12:27:58 +0300

    real world use of logger

---
+++ Logging practical Implementation In python/app.py
@@ -0,0 +1,43 @@
+import logging
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S',
+    force=True,
+    handlers=[
+        logging.FileHandler('app1.log'), 
+        logging.StreamHandler()
+    ]
+)
+
+logger = logging.getLogger("ArithmeticApp")
+
+def add(a, b):
+    result = a + b
+    logger.debug(f"Adding {a} and {b} gives {result}")
+    return result
+
+def subtract(a, b):
+    result = a - b
+    logger.debug(f"Subtracting {b} from {a} gives {result}")
+    return result
+
+def multiply(a, b):
+    result = a * b
+    logger.debug(f"Multiplying {a} and {b} gives {result}")
+    return result
+
+def divide(a, b):
+    try:
+        result = a / b
+        logger.debug(f"Dividing {a} by {b} gives {result}")
+        return result
+    except ZeroDivisionError:
+        logger.error("Cannot divide by zero")
+        return None
+    
+add(10, 5)
+subtract(10, 5) 
+multiply(10, 5) 
+divide(10, 3)
